53006ddc1414d608ff4a154efeffeb7497245964
Simman102 <simman102@gmail.com>
2025-10-06 16:38:15 +0200

    add prettier

--- src/MenuApp/App.tsx
+++ src/MenuApp/App.tsx
@@ -1,6 +1,6 @@
-import {Grid, Typography} from "@mui/material";
-import {useState} from "react";
-import AreaButtonPair from "./components/AreaButtonPair";
+import { Grid, Typography } from '@mui/material';
+import { useState } from 'react';
+import AreaButtonPair from './components/AreaButtonPair';
 
 export default function App() {
     const [isLeftHidden, setLeftHidden] = useState(false);
@@ -8,32 +8,34 @@ export default function App() {
     const [leftBodyFields, setLeftBodyFields] = useState([]);
     const [rightBodyFields, setRightBodyFields] = useState([]);
 
-    return <Grid container spacing={2}>
-        <Grid size={12}>
-            <Typography variant={"h3"} align={"center"}>
-                Tot-comparator
-            </Typography>
+    return (
+        <Grid container spacing={2}>
+            <Grid size={12}>
+                <Typography variant={'h3'} align={'center'}>
+                    Tot-comparator
+                </Typography>
+            </Grid>
+            <Grid size={12}>
+                <Typography variant={'h6'} align={'center'}>
+                    Input valid Tot strings in both fields.
+                </Typography>
+            </Grid>
+            <Grid size={6}>
+                <AreaButtonPair
+                    isHidden={isLeftHidden}
+                    setHidden={setLeftHidden}
+                    oppositeBodyFields={rightBodyFields}
+                    setOwnBodyFields={setLeftBodyFields}
+                />
+            </Grid>
+            <Grid size={6}>
+                <AreaButtonPair
+                    isHidden={isRightHidden}
+                    setHidden={setRightHidden}
+                    oppositeBodyFields={leftBodyFields}
+                    setOwnBodyFields={setRightBodyFields}
+                />
+            </Grid>
         </Grid>
-        <Grid size={12}>
-            <Typography variant={"h6"} align={"center"}>
-                Input valid Tot strings in both fields.
-            </Typography>
-        </Grid>
-        <Grid size={6}>
-            <AreaButtonPair
-                isHidden={isLeftHidden}
-                setHidden={setLeftHidden}
-                oppositeBodyFields={rightBodyFields}
-                setOwnBodyFields={setLeftBodyFields}
-            />
-        </Grid>
-        <Grid size={6}>
-            <AreaButtonPair
-                isHidden={isRightHidden}
-                setHidden={setRightHidden}
-                oppositeBodyFields={leftBodyFields}
-                setOwnBodyFields={setRightBodyFields}
-            />
-        </Grid>
-    </Grid>
+    );
 }
--- src/MenuApp/components/AreaButtonPair.tsx
+++ src/MenuApp/components/AreaButtonPair.tsx
@@ -1,49 +1,53 @@
-import React, {FormEvent, useState} from "react";
-import {Button, Stack, TextareaAutosize, Typography} from "@mui/material";
-import {BODY_FIELDS_LENGTH, bodyFields} from "./constants";
+import React, { FormEvent, useState } from 'react';
+import { Button, Stack, TextareaAutosize, Typography } from '@mui/material';
+import { BODY_FIELDS_LENGTH, bodyFields } from './constants';
 
 export default function AreaButtonPair({
-                                           isHidden,
-                                           setHidden,
-                                           oppositeBodyFields,
-                                           setOwnBodyFields
-                                       }: AreaButtonPairProps) {
+    isHidden,
+    setHidden,
+    oppositeBodyFields,
+    setOwnBodyFields,
+}: AreaButtonPairProps) {
     const [currentBodyFields, setCurrentBodyFields] = useState([]);
 
-    return <Stack spacing={2} direction={"column"}>
-        {isHidden && <Button
-            hidden={isHidden}
-            onClick={() => setHidden(!isHidden)}
-            variant={"outlined"}
-        >RESTORE FIELD</Button>}
-        {isHidden &&
-            currentBodyFields.map((value, index) => {
-                const color = (oppositeBodyFields[index] === value || oppositeBodyFields.length === 0) ?
-                    null : "error";
-                return <Typography
-                    color={color}>
-                    {bodyFields[index]}: {value}
-                </Typography>
-            })
-        }
-        {!isHidden && <TextareaAutosize
-            onInput={(event: FormEvent<HTMLTextAreaElement>) => {
-                const currentFields = event.currentTarget.value.split("|");
+    return (
+        <Stack spacing={2} direction={'column'}>
+            {isHidden && (
+                <Button hidden={isHidden} onClick={() => setHidden(!isHidden)} variant={'outlined'}>
+                    RESTORE FIELD
+                </Button>
+            )}
+            {isHidden &&
+                currentBodyFields.map((value, index) => {
+                    const color =
+                        oppositeBodyFields[index] === value || oppositeBodyFields.length === 0 ? null : 'error';
+                    return (
+                        <Typography color={color}>
+                            {bodyFields[index]}: {value}
+                        </Typography>
+                    );
+                })}
+            {!isHidden && (
+                <TextareaAutosize
+                    onInput={(event: FormEvent<HTMLTextAreaElement>) => {
+                        const currentFields = event.currentTarget.value.split('|');
 
-                if (currentFields.length === BODY_FIELDS_LENGTH) {
-                    setHidden(!isHidden);
-                    setCurrentBodyFields(currentFields);
-                    setOwnBodyFields(currentFields);
-                }
-            }}
-            minRows={3}
-        ></TextareaAutosize>}
-    </Stack>
+                        if (currentFields.length === BODY_FIELDS_LENGTH) {
+                            setHidden(!isHidden);
+                            setCurrentBodyFields(currentFields);
+                            setOwnBodyFields(currentFields);
+                        }
+                    }}
+                    minRows={3}
+                ></TextareaAutosize>
+            )}
+        </Stack>
+    );
 }
 
 interface AreaButtonPairProps {
-    isHidden: boolean,
-    setHidden: React.Dispatch<React.SetStateAction<boolean>>,
-    oppositeBodyFields: string[],
-    setOwnBodyFields: React.Dispatch<React.SetStateAction<string[]>>,
+    isHidden: boolean;
+    setHidden: React.Dispatch<React.SetStateAction<boolean>>;
+    oppositeBodyFields: string[];
+    setOwnBodyFields: React.Dispatch<React.SetStateAction<string[]>>;
 }
